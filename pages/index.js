6bd88d6280548523ca7a93aa17677ffe9b75e4ce
NikBel3476 <nikita.belkin3476@gmail.com>
2022-07-01 15:37:45 +0400

    chore: format .js files

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
-import { Badge } from "react-bootstrap";
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+import { Badge } from 'react-bootstrap';
 
 export default function Home() {
-  return (
-    <h2>
-      Main page
-      <Badge bg="secondary">Badge</Badge>
-    </h2>
-  );
+	return (
+		<h2>
+			Main page
+			<Badge bg="secondary">Badge</Badge>
+		</h2>
+	);
 }
